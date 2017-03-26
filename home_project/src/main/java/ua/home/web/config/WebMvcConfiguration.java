b57e24b71b72f126f002f73c8ba03b733a8afc91
Anton Tarianyk <anton_tarianyk@epam.com>
2017-03-26 18:16:23 +0300

    added file config

--- home_project/src/main/java/ua/home/web/config/WebMvcConfiguration.java
+++ home_project/src/main/java/ua/home/web/config/WebMvcConfiguration.java
@@ -1,5 +1,6 @@
 package ua.home.web.config;
 
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
@@ -7,13 +8,39 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import javax.servlet.MultipartConfigElement;
+import java.io.File;
+
 @Configuration
 @EnableWebMvc
 public class WebMvcConfiguration extends WebMvcConfigurerAdapter {
 
+    public static final String DIRECTORY = "E:\\images\\";
+
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize("10MB");
+        factory.setMaxRequestSize("10MB");
+        createDirectory();
+        factory.setLocation(DIRECTORY);
+        return factory.createMultipartConfig();
+    }
+
+    private void createDirectory() {
+        File file = new File(DIRECTORY);
+        if (!file.exists()) {
+            if (file.mkdir()) {
+                System.out.println("Directory is created!");
+            } else {
+                System.out.println("Failed to create directory!");
+            }
+        }
     }
 
     @Bean
